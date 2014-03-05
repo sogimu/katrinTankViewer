b00d27fad254a856b2be725004b76f8c7de28f3d
Aleksander Lizin <sogimu@nxt.ru>
2014-03-05 10:21:16 +0100

    Fixed .../doc... to .../utils... in generateDocs.sh

--- generateDocs.sh
+++ generateDocs.sh
@@ -4,4 +4,4 @@ PATH_TO_CODE="./sources"
 PATH_TO_JSDOC="./utils/jsdoc/jsdoc.js"
 PATH_TO_DOCS_FILE="./doc"
 
-python ./lib/generateDocs.py "$PATH_TO_CODE" "$PATH_TO_DOCS_FILE" "$PATH_TO_JSDOC"
+python ./utils/generateDocs.py "$PATH_TO_CODE" "$PATH_TO_DOCS_FILE" "$PATH_TO_JSDOC"
